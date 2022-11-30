d073546f2b58c7c211f9c673b222de3551d8db58
sofi <bedlusofi@gmail.com>
2022-11-30 16:36:59 +0000

    function fuly modifiad

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,18 +7,50 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+	char **addres = NULL, **pr;
+	listint_t *temp;
+	int size = 3, tp = 0, i = 0;
 
-	if (list == NULL)
+	if (list ==NULL)
 		return (0);
-	slow = list;
-	fast = list->next;
-	while (slow != NULL && fast->next != NULL && fast != NULL)
+	addres = malloc(sizeof(char *) * size);
+	if (addres == NULL)
 	{
-		if (slow == fast)
-			return (1);
-		slow = slow->next;
-		fast = fast->next->next;
+		printf("can't allocate memory");
+		exit(EXIT_FAILURE);
 	}
+	temp = list;
+	while(temp != NULL)
+	{
+		addres[i] = (char *)temp;
+		if (i == size - 1)
+		{
+			size += size;
+			pr = realloc(addres, sizeof(char *) * size);
+			if (!pr)
+			{
+				printf("allocation error");
+				exit(EXIT_FAILURE);
+			}
+			addres = pr;
+		}
+		if (i > 0)
+		{
+			tp = i;
+			while(tp >= 0)
+			{
+				if ((char *)(temp->next) == addres[tp])
+				{
+					free(addres);
+					return (1);
+				}
+				tp--;
+			}
+
+		}
+		temp = temp->next;
+		i++;
+	}
+	free(addres);
 	return (0);
 }
