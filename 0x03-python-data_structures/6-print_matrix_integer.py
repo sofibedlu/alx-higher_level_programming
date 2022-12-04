fa2c41018e7e139a3574530c16ecb630b1ea5637
sofi <bedlusofi@gmail.com>
2022-12-04 16:46:47 +0000

    last space removed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/pythton3
 def print_matrix_integer(matrix=[[]]):
+    size = len(matrix[0])
     for i in matrix:
+        col = 0
         for j in i:
-            print("{:d} ".format(j), end="")
+            if col == size - 1:
+                print("{:d}".format(j), end="")
+            else:
+                print("{:d} ".format(j), end="")
+            col += 1
         print()
