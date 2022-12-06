18a1742782a8d7cb1df67e2303b6ecff5c4d93e7
sofi <bedlusofi@gmail.com>
2022-12-06 14:52:46 +0000

    task 13 completed

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+/**
+ * insert_node - insert nod in sorted linked list
+ * @head: head of the list
+ * @number: the number that will be added to the list
+ * Return: head of the new list
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temp, *prev;
+	listint_t *insert;
+
+	if (*head == NULL)
+		return (NULL);
+	insert = malloc(sizeof(listint_t));
+	if (insert == NULL)
+	{
+		printf("malloc faild\n");
+		return (NULL);
+	}
+	insert->n = number;
+	insert->next = NULL;
+	temp = (*head)->next;
+	prev = *head;
+	if ((*head)->n > insert->n)
+	{
+		insert->next = *head;
+		*head = insert;
+	}
+	else
+	{
+		while (temp != NULL)
+		{
+			if (temp->n > insert->n)
+			{
+				insert->next = temp;
+				prev->next = insert;
+				break;
+			}
+			prev = temp;
+			temp = temp->next;
+			if (temp == NULL)
+			{
+				prev->next = insert;
+				break;
+			}
+		}
+	}
+	return (*head);
+}
