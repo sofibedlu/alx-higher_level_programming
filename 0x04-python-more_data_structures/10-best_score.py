c28fcdd90f742c0c5f93250c832894986b421015
sofi <bedlusofi@gmail.com>
2022-12-07 15:55:10 +0000

    check if score int

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,6 +5,7 @@ def best_score(a_dictionary):
     key = list(a_dictionary)[0]
     max_s = a_dictionary[key]
     for i in a_dictionary:
-        if max_s < a_dictionary[i]:
-            max_s = a_dictionary[i]
+        if isinstance(a_dictionary[i], int):
+            if max_s < a_dictionary[i]:
+                max_s = a_dictionary[i]
     return max_s
