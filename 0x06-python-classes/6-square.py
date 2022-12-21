a16d6279e3990ad777d177d71e9323da445181d0
sofi <bedlusofi@gmail.com>
2022-12-21 17:18:56 +0000

    fixing bug

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,8 +10,8 @@ class Square:
                 size (int): size of the square
                 position (int, int): coordinates on the square
             """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
