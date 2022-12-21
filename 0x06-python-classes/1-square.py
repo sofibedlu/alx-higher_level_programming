cd04ccd800797e396c22761a6980ef627126f95a
sofi <bedlusofi@gmail.com>
2022-12-21 13:40:55 +0000

    task-1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Define Suare class"""
+
+
+class Square:
+    """Represent a square"""
+    def __init__(self, size):
+        """
+        Args:
+            size (unkown): the size of the square
+            """
+        self.__size = size
