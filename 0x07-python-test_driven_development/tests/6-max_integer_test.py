1f2222dcb592c5bd4807252a5658072c5ca90f42
sofi <bedlusofi@gmail.com>
2023-01-04 18:09:14 +0000

    unittest updated

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,7 +11,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer(list1), 89)
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([-1, -56, 0, -100]), 0)
-        self.assertEqual(max_integer([1, 2, 3, 4]), 3)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([4]), 4)
         self.assertEqual(max_integer([1.3, 0.66, 6.1, -1.9]), 6.1)
 
