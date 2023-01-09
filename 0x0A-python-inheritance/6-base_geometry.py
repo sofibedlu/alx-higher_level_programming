91c69d6e996ba2cba6cd0f2bec759b7c8f1b52c3
sofi <bedlusofi@gmail.com>
2023-01-09 17:36:11 +0000

    task-6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define class BaseGeometry"""
+
+
+class BaseGeometry:
+    """represent geometry instance"""
+    def area(self):
+        raise Exception('area() is not implemented')
