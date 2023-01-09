41bccd9c6dc2f516d965e1c54b935cb3e1ab76ba
sofi <bedlusofi@gmail.com>
2023-01-09 18:21:17 +0000

    task-7 and documentation

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define class BaseGeometry"""
+
+
+class BaseGeometry:
+    """represent geometry instance"""
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if not isinstance(name, str):
+            raise TypeError("name must be string")
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
