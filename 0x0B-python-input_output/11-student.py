caee0b30f2abed067f03ffe73b7a2cddb409b285
sofi <bedlusofi@gmail.com>
2023-01-11 11:34:53 +0000

    task-11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Define a class Student"""
+
+
+class Student:
+    """defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """initialize the instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """it retrieves a dictionary representation of a Student instance
+        Args:
+        attrs (list): list of strings(keys to the dictionary)
+        """
+        if (isinstance(attrs, list) and
+                all(isinstance(ele, str) for ele in attrs)):
+            dic = vars(self)
+            new_dic = {}
+            for key in attrs:
+                try:
+                    dic[key]
+                except KeyError:
+                    continue
+                new_dic[key] = dic[key]
+            return new_dic
+        return vars(self)
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance
+        Args:
+            json (dict): dictionary object from json file
+        """
+        if type(json) == dict:
+            self.__dict__ = json
