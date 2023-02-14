ff853be12eeb1045c9d4641f64f57114cac9d068
sofi <bedlusofi@gmail.com>
2023-02-14 19:16:13 +0000

    full description on table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- prints full description of the table 'first_table' from the database 'hbtn_0c_0'
+-- the database name will be passed an argumnet of the mysql command
+SHOW CREATE TABLE first_table
