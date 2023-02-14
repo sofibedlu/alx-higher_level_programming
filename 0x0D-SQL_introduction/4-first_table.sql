ec26e4d2306473272b101f3d7852e7b46e286e4d
sofi <bedlusofi@gmail.com>
2023-02-14 18:50:46 +0000

    create table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- creates as table 'first_table' in current database in my mysql server
+-- database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
