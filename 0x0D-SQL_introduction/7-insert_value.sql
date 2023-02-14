0ecb25425f48c430702509730b2261adbd540bf0
sofi <bedlusofi@gmail.com>
2023-02-14 19:30:22 +0000

    insert

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- insert new row in the table 'first_table' database hbtn_0c_0 in my mysql server
+-- database name will be passed as an argument of the mysql command
+INSERT INTO first_table
+VALUES (89, 'Best School')
