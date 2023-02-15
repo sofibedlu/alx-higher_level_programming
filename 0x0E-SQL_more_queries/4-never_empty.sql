16d745d4fe940a90b489b479dbbfdec9a02919c8
sofi <bedlusofi@gmail.com>
2023-02-15 21:21:47 +0000

    task-4

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- creates a table 'id_not_null'on my mysql
+-- the database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id INT DEFAULT 1,
+	name VARCHAR(256)
+);
