e0e0615286f48bbc8cc9d4ea47566eb204331e15
sofi <bedlusofi@gmail.com>
2023-02-22 19:35:27 +0000

    foreign-key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,15 @@
+-- creates the database 'hbtn_0d_usa' and the table 'cities' (inside the database hbtn_0d_usa)
+-- in mysql server
+-- 'cities' description:
+--	id INT unique, auto generated, can't be null and is a primary key
+--	state_id INT, can't be null and must be a foreign key that reference to id of states tabl
+--	name VARCHAR(256) can't be null
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
