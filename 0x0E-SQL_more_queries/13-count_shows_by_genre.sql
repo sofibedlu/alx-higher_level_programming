b5903f25bbeff6f24984a7c352c0702708de8d9c
sofi <bedlusofi@gmail.com>
2023-03-06 18:30:25 +0000

    update

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,7 +6,7 @@
 -- Results sorted in descending order by the number of shows linked
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name as genre, COUNT(*) as number_of_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
