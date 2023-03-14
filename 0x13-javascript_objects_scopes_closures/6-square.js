66f7b6fb8782106dd4fad850129b80c92975d45e
sofi <bedlusofi@gmail.com>
2023-03-14 18:33:14 +0000

    update-3

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const SquareP = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends SquareP {
   constructor (size) {
     super(size, size);
   }
 
   charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        process.stdout.write(c);
+    if (typeof c === 'undefined') {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        console.log('');
       }
-      console.log('');
     }
   }
 }
