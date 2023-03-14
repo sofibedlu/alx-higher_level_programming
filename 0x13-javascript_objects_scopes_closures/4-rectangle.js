f5a88d1bf56b3ec8415ae09895d19d7c94c3d9e5
sofi <bedlusofi@gmail.com>
2023-03-14 16:18:55 +0000

    task-4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w === 0 || !Number.isInteger(w) || w < 0 ||
+         h === 0 || !Number.isInteger(h) || h < 0) {
+      return this;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+
+  rotate () {
+    const current = this.width;
+    this.width = this.height;
+    this.height = current;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
