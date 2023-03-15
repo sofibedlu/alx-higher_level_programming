8200e23c3909ed070eb1b2ca7237e4b890277fbe
sofi <bedlusofi@gmail.com>
2023-03-15 19:22:22 +0000

    fix-for-duplicate-list

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,15 @@
 #!/usr/bin/node
 
 const list = require('./100-data').list;
+
+const indices = [];
 const newList = list.map((x) => {
+  const j = list.indexOf(x);
+  if (j in indices) {
+    indices.push(indices.length);
+    return x * (indices.length);
+  }
+  indices.push(j);
   return x * list.indexOf(x);
 });
 console.log(list);
