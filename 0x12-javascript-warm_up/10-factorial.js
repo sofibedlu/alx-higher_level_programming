489add68aaf5acd0da1ee2e7d2141d83fdb744ee
sofi <bedlusofi@gmail.com>
2023-03-15 09:08:57 +0000

    task-10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function factorial (value) {
+  if (value == 1) {
+    return 1;
+  }
+  return value * factorial(value - 1);
+}
+const num = process.argv[2];
+if (isNaN(num)) {
+  console.log(1);
+} else {
+  const facto = factorial(parseInt(num));
+  console.log(facto);
+}
