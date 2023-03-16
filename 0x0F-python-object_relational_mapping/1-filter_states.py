0af1d341a6ea140470b9d48f1c23e466b8501611
sofi <bedlusofi@gmail.com>
2023-03-16 21:07:16 +0000

    task-3 and update-prev

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,14 +9,16 @@
 import MySQLdb
 from sys import argv
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                       passwd=argv[2], db=argv[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_re = cur.fetchall()
-for row in query_re:
-    if row[1][0] == 'N':
-        print(row)
+if __name__ == "__main__":
 
-cur.close()
-conn.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_re = cur.fetchall()
+    for row in query_re:
+        if row[1][0] == 'N':
+            print(row)
+
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 
-"""a script that takes in an argument and displays all values in the
+"""-a script that takes in an argument and displays all values in the
     'states' table of hbtn_0e_0_usa where 'name' matches the argument.
-    script take 4 arguments: mysql username, mysql password, database
+    -check the argument is safe from SQL injections
+    -script take 4 arguments: mysql username, mysql password, database
     name and state name searched
 """
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset='utf8')
     cur = conn.cursor()
+
     cur.execute("SELECT * FROM states\
-                WHERE name = '{}'\
-                ORDER BY states.id ASC".format(argv[4]))
+                WHERE name = %s\
+                ORDER BY states.id ASC", (argv[4],))
 
     query_re = cur.fetchall()
     for row in query_re:
         print(row)
-
     cur.close()
     conn.close()
