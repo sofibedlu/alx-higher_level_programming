e0f01a19259ef5d726cfb0b699b36578626d5c29
sofi <bedlusofi@gmail.com>
2023-03-17 15:55:47 +0000

    update-filter-method

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
     session = Session(engine)
 
-    row = session.query(State).filter(State.id == 1)
+    row = session.query(State).first()
     if not row:
         print("")
     else:
-        print("{}: {}".format(row[0].id, row[0].name))
+        print("{}: {}".format(row.id, row.name))
     session.close()
