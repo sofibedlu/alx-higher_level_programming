e43d90974be95527d58a4a660af77235467be938
sofi <bedlusofi@gmail.com>
2023-03-17 12:21:39 +0000

    task-6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""a script that contains the class definition of a 'State' and
+    an instance 'Base = declarative_base()'
+"""
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """define the table called 'states'"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
