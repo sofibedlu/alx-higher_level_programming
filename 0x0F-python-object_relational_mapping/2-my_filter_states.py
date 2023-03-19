8723bcf8b07e89c439ab17c55d94d51bb00836cb
sofi <bedlusofi@gmail.com>
2023-03-19 16:39:51 +0000

    check the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                            passwd=argv[2], db=argv[3], charset='utf8')
     cur = conn.cursor()
     cur.execute("SELECT * FROM states\
-                WHERE name = '{}'\
+            WHERE name LIKE '{:s}'\
                 ORDER BY states.id ASC".format(argv[4]))
 
     query_re = cur.fetchall()
