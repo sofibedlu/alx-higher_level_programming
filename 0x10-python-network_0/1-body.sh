431828b75e9ebd9a391c257c6bcf8ab60371ef9a
sofi <bedlusofi@gmail.com>
2023-03-30 18:31:02 +0000

    task-1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL, sends a GET request to the URL, and displays the body of 200 staus ./ code response
-curl -s "$1"
+curl -sI "$1" | grep -q "HTTP/1.1 200 OK" && curl -s "$1"
