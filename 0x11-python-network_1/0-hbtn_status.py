8b164c745e4fb638001cd88dc91c5002aef134d5
sofi <bedlusofi@gmail.com>
2023-03-31 11:11:24 +0000

    intial-commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as re:
+        content = re.read()
+        utf = content.decode('utf-8')
+        print("Body response:")
+        print("     - type: {}".format(type(content)))
+        print("     - content: {}".format(content))
+        print("     - utf8 content: {}".format(utf))
