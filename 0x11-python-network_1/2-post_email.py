1e7196afe8133269e685ac2980656e82992d5f94
sofi <bedlusofi@gmail.com>
2023-03-31 14:23:54 +0000

    task-2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email, sends a POST request to the
+    passed URL with the email as a parameter, and displays the body of
+    the response (decoded in utf-8)
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    value = {'email': sys.argv[2]}
+    value = urllib.parse.urlencode(value)
+    data = value.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read())
