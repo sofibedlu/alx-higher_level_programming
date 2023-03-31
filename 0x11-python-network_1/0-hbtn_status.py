eeadac23efb668c04ac2c45371390544fbb4aebe
sofi <bedlusofi@gmail.com>
2023-03-31 11:18:38 +0000

    add try cluase

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,14 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as re:
-        content = re.read()
-        utf = content.decode('utf-8')
-        print("Body response:")
-        print("     - type: {}".format(type(content)))
-        print("     - content: {}".format(content))
-        print("     - utf8 content: {}".format(utf))
+    try:
+        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+                as re:
+            content = re.read()
+            utf = content.decode('utf-8')
+            print("Body response:")
+            print("     - type: {}".format(type(content)))
+            print("     - content: {}".format(content))
+            print("     - utf8 content: {}".format(utf))
+    except urllib.error.URLError as e:
+        print(e.reason)
