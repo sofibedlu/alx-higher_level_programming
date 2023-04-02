84e93fc3c2d24d5652d973475f23827203fabf36
sofi <bedlusofi@gmail.com>
2023-04-02 18:52:18 +0000

    update-condn

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,10 +7,10 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        data = {'q': sys.argv[1]}
-    else:
+    if len(sys.argv) == 1:
         data = {'q': ""}
+    else:
+        data = {'q': sys.argv[1]}
     r = requests.post('http://0.0.0.0:5000/search_user', data)
     try:
         jre = r.json()
